de4b47596b4985f7602e0b306fb385d46c63599b
estevanelectronicsmedia <156565220+estevanelectronicsmedia@users.noreply.github.com>
2025-08-27 14:22:49 -0700

    Create tb.sv

---
+++ activities/01-Reporting/src/tb.sv
@@ -0,0 +1,11 @@
+// Activity 01:
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+ 
+module tb;
+  initial begin
+    `uvm_info("Testbench", $sformatf("First RTL Code : %0s","Half Adder"), UVM_NONE);
+  end
+  
+endmodule
